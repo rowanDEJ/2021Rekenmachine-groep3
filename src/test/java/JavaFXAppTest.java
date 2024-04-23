3b305adf9be1eb879569a400d4effcb6f2f533a2
ABHHS <23091088@student.hhs.nl>
2024-04-23 10:08:18 +0200

    computeAdd

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,11 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+    @Test
+    void AddTest() {
+        JavaFXApp app = new JavaFXApp();
+        assertEquals(0, app.computeAdd(0, 0));
+        assertEquals(1, app.computeAdd(4, -3));
+        assertEquals(-8, app.computeAdd(-5, -3));
+    }
 }
