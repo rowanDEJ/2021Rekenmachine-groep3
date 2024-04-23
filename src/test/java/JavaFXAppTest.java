8bc8684817f1d8db5abd18e2da02612d521042e2
rowanDEJ <rowandejong06@gmail.com>
2024-04-23 10:15:36 +0200

    Divide function + Test 2

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,12 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+
+    @Test
+    void testComputeDivide() {
+        JavaFXApp app = new JavaFXApp();
+        assertEquals(2, app.computeDivide(4, 2));
+        assertEquals(4, app.computeDivide(8, 2));
+        assertEquals(3, app.computeDivide(9, 3));
+    }
 }
