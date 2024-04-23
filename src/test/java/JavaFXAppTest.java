45c591e1868c0d53623458f0d67d30206ec6f00f
MeneerMart <102212872+MeneerMart@users.noreply.github.com>
2024-04-23 10:02:21 +0200

    Test voor het vermenigvuldigen toegevoegd

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -4,6 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class JavaFXAppTest {
     @Test
     void aDummyTest() {
-        assertEquals(0,0);
+        assertEquals(0, 0);
+    }
+
+    @Test
+    void testComputeMultiply() {
+        JavaFXApp app = new JavaFXApp();
+        assertEquals(0, app.computeMultiply(0, 0));
+        assertEquals(-12, app.computeMultiply(4, -3));
+        assertEquals(15, app.computeMultiply(-5, -3));
     }
 }
